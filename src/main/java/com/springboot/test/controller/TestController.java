37d47c8e0e752f0cccd83bbb65003a0e8533fd94
6feetlife <6feetlife56@gmail.com>
2025-04-11 10:52:52 +0900

    test sample controller

---
+++ src/main/java/com/springboot/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.springboot.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping
+    public  String helloLogBeI(){
+        return "Hello Log_Be_I";
+    }
+}
