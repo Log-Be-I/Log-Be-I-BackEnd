332441ed5370144e3e07848a7beb16344c05d3df
6feetlife <6feetlife56@gmail.com>
2025-04-12 19:08:15 +0900

    keyword entity

---
+++ src/main/java/com/springboot/keyword/entity/Keyword.java
@@ -0,0 +1,44 @@
+package com.springboot.keyword.entity;
+
+import com.springboot.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Keyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long keywordId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private KeywordStatus keywordStatus = KeywordStatus.KEYWORD_REGISTERED;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public enum KeywordStatus {
+        KEYWORD_REGISTERED("키워드 등록"),
+        KEYWORD_DELETED("키워드 삭제");
+
+        @Setter
+        private String status;
+
+        KeywordStatus(String status) {
+            this.status = status;
+        }
+    }
+
+}
