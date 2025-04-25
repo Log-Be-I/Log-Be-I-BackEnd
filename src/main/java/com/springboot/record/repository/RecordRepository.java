221b40b3a23669f9d59ca22e6d82a754370512c2
6feetlife <6feetlife56@gmail.com>
2025-04-26 01:24:45 +0900

    Update RecordRepository.java

--- src/main/java/com/springboot/record/repository/RecordRepository.java
+++ src/main/java/com/springboot/record/repository/RecordRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
