49b26d9636803fa75c3431150cb4bf70381c4ae3
6feetlife <6feetlife56@gmail.com>
2025-04-26 01:01:09 +0900

    log-s3

--- src/main/java/com/springboot/log/LogStorageService.java
+++ src/main/java/com/springboot/log/LogStorageService.java
@@ -12,11 +12,31 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class LogStorageService {
 
+    // redisTemplate DI
     private final RedisTemplate<String, String> redisTemplate;
 
-    // Logback -> Appender 를 커스터마이징해서 로그를 Redis 로 보내는 방식
+    // 로그 메시지를 날짜별로 redis 에 저장하는 역할
     public void storeInfoLog(String message) {
-        String key = "logs:" + LocalDate.now();  // 날짜 단위로 묶음
-        redisTemplate.opsForList().rightPush(key, message);
+        try{
+            // 생성되는 로그의 키값은 당일 날짜로 한정 -> 날짜별로 같은 키값 공유
+            String key = "logs:" + LocalDate.now();  // 날짜 단위로 묶음
+            // redis 의 list 자료구조에 접근하여
+            // 해당 날짜 key (logs: yyyy-MM-dd)의 리스트 맨 끝에 메시지를 추가 저장함
+            // key 는 곧 특정 리스트의 이름, message 는 단지 해당 리스트에 쌓일 뿐
+            redisTemplate.opsForList().rightPush(key, message);
+        } catch (Exception e) {
+            logAndStoreWithError("Redis log save failed: {}", e.getMessage());
+        }
+
+    }
+    // ✔️ 콘솔 + Redis 저장 한 번에
+    public void logAndStoreWithError(String message, String eMessage) {
+        log.info(message);
+        storeInfoLog(message);
+    }
+
+    public void logAndStore(String message) {
+        log.info(message);
+        storeInfoLog(message);
     }
 }
--- src/main/java/com/springboot/log/LogTransferScheduler.java
+++ src/main/java/com/springboot/log/LogTransferScheduler.java
@@ -1,5 +1,6 @@
 package com.springboot.log;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -17,27 +19,43 @@ public class LogTransferScheduler {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final AmazonS3 amazonS3;
-    private final String bucketName = "tour-bucket-name";
+    private final String bucketName = "logbe-i-log";
+    private final LogStorageService logStorageService;
 
-    // 매시 정각 (0초 0분 *시 *일 *월 *요일)
-    @Scheduled(cron = "0 0 * * * *")
+    // 하루 1회(24시간) 이벤트 발생(0초 0분 0시 *일 *월 *요일)
+//    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public void transferLogsToS3() {
 
         // 현재 날짜에서 -1 day => 어제
         String key = "logs:" + LocalDate.now().minusDays(1);
+//        String key = "logs:" + LocalDate.now();
+        // redis 에서 List 형태로 첫 기록부터 마지막 기록까지 다 가져오겠다는 의미
         List<String> logs = redisTemplate.opsForList().range(key, 0, -1);
 
+        // 뽑아온 log 목록이 null 아니거나 비어있지 않다면
         if (logs != null && !logs.isEmpty()) {
+            // log 리스트를 하나의 문자열로 합치는 코드
             String logData = String.join("\n", logs);
 
             // S3 업로드
+            // 로그 파일의 경로와 이름을 지정하는 코드
             String fileName = "logs/" + key + ".log";
-            amazonS3.putObject(bucketName, fileName, logData);
+            try {
+                // S3에 실제 로그 데이터를 업로드하는 코드 (버킷 이름, 파일 경로, 저장하려는 로그 문자열)
+                amazonS3.putObject(bucketName, fileName, logData);
+            } catch (AmazonServiceException e) {
+                logStorageService.logAndStore("S3 upload failed");
+                return;
+            }
 
             // Redis 로그 삭제
+            // key 값으로 들어오느 값과 같은 키를 가진 데이터 전부 삭제
             redisTemplate.delete(key);
 
-            log.info("✅ 로그 이관 완료: {}", fileName);
+            // 파일 경로와 함께 이관 로그 찍기
+            log.info("Log transfer completed: {}", fileName);
         }
     }
+
 }
