a329dd16ea4481864823fc6d9f0f08ca4480679f
6feetlife <6feetlife56@gmail.com>
2025-04-29 20:00:05 +0900

    mapper localDateTime 변환

--- src/main/java/com/springboot/schedule/mapper/ScheduleMapper.java
+++ src/main/java/com/springboot/schedule/mapper/ScheduleMapper.java
@@ -6,13 +6,30 @@ import com.springboot.schedule.dto.ScheduleResponseDto;
 import com.springboot.schedule.entity.Schedule;
 import org.mapstruct.Mapper;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ScheduleMapper{
-    Schedule schedulePostDtoToSchedule (SchedulePostDto schedulePostDto);
-    Schedule schedulePatchDtoToSchedule (SchedulePatchDto schedulePatchDto);
+    default Schedule schedulePostDtoToSchedule (SchedulePostDto schedulePostDto){
+        Schedule schedule = new Schedule();
+        schedule.setTitle(schedule.getTitle());
+        schedule.setStartDateTime(postStringToLocalDateTime(schedulePostDto.getStartDateTime()));
+        schedule.setEndDateTime(postStringToLocalDateTime(schedulePostDto.getEndDateTime()));
+
+        return schedule;
+    }
+
+    default Schedule schedulePatchDtoToSchedule (SchedulePatchDto schedulePatchDto){
+        Schedule schedule = new Schedule();
+        schedule.setTitle(schedule.getTitle());
+        schedule.setStartDateTime(postStringToLocalDateTime(schedulePatchDto.getStartDateTime()));
+        schedule.setEndDateTime(postStringToLocalDateTime(schedulePatchDto.getEndDateTime()));
+
+        return schedule;
+    }
     default ScheduleResponseDto scheduleToscheduleResponseDto (Schedule schedule) {
         ScheduleResponseDto responseDto = new ScheduleResponseDto();
         responseDto.setScheduleId(schedule.getScheduleId());
@@ -31,4 +48,10 @@ public interface ScheduleMapper{
         return scheduleResponseDtos;
     }
 
+    //PatchDto의 recordDateTime (String -> LocalDateTime) 타입 변환 메서드
+    default LocalDateTime postStringToLocalDateTime(String dateTimeStr){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return LocalDateTime.parse(dateTimeStr, formatter);
+
+    }
 }
