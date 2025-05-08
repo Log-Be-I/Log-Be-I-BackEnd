23367a97234584762b6ee0515fb245b2ede9b46f
6feetlife <6feetlife56@gmail.com>
2025-05-08 17:13:14 +0900

    swagger schedule

--- src/main/java/com/springboot/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/com/springboot/schedule/dto/ScheduleResponseDto.java
@@ -13,27 +13,30 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(description = "일정 응답 DTO")
 public class ScheduleResponseDto {
-//    @Schema(description = "일정 Id", example = "1")
+
+    @Schema(description = "일정 Id", example = "1")
     private Long scheduleId;
 
-//    @Schema(description = "제목", example = "점심점심")
     @NotBlank
+    @Schema(description = "제목", example = "오늘 점심은 한식")
     private String title;
 
-//    @Schema(description = "시작 날짜", example = "2025-04-12T13:30")
     @NotBlank
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDateTime;
 
-//    @Schema(description = "종료 날짜", example = "2025-04-12T14:30")
     @NotBlank
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @Schema(description = "종료 날짜", example = "2025-04-12T14:30")
     private LocalDateTime endDateTime;
 
+    @Schema(description = "생성일", example = "2025-04-11T11:30")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
 
+    @Schema(description = "수정일", example = "2025-04-11T11:30")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime modifiedAt;
 }
