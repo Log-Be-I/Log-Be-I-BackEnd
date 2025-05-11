4178b8d27caf5575f37889e95364caaf5b4a4b6c
6feetlife <6feetlife56@gmail.com>
2025-05-11 11:08:30 +0900

    dashboard swagger 작성 및 aws, logs 출력 안되게 설정

--- src/main/java/com/springboot/dashboard/controller/DashBoardController.java
+++ src/main/java/com/springboot/dashboard/controller/DashBoardController.java
@@ -13,10 +13,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/offices")
